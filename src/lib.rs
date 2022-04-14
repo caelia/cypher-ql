0c42c01cb6382dc83cc9178ab247559fa01330e2
Caelia R Chapin <crc@caelia.net>
2022-04-13 18:42:04 -0600

    Added README.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub mod parser;
+pub mod processor;
+mod types;
+
+pub trait QueryProcessor<TConn> {
+    fn execute(conn: TConn, query: types::Query) -> types::QueryResult;
 }
